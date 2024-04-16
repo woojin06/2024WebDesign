20e50bfb99220c61f393c378c8357f246cdfc07a
woojin06 <101300158+woojin06@users.noreply.github.com>
2024-04-16 10:50:15 +0900

    js ing

---
+++ script.js
@@ -0,0 +1,12 @@
+$(".main-menu li").mouseover(function(){
+    $(".sub-menu").stop().slideDown()
+}).mouseout(function(){
+    $(".sub-menu").stop().slideUp();
+})
+
+$(".tab-title ul li:first-child").click
+(function(){
+    $(".galley .tab-con").hide();
+    $(".con-box" .tab-con).show();
+})
+$(".tab-title ul li:nth-chlid(2)").click
