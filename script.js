e7aca8f907da8bc68789b16c81d961c6e03c3b3d
woojin06 <101300158+woojin06@users.noreply.github.com>
2024-04-22 15:28:33 +0900

    2024-04-22

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+//슬라이드 메뉴
 $(".main-menu li").mouseover(function(){
-    $(".sub-menu").stop().slideDown()
+    $(".sub-menu").stop().slideDown();
 }).mouseout(function(){
     $(".sub-menu").stop().slideUp();
 })
-
-$(".tab-title ul li:first-child").click
+//공지사항 탭 버튼 클릭했을 때
+$(".tab-title2 ul li:first-child").click
 (function(){
-    $(".galley .tab-con").hide();
-    $(".con-box" .tab-con).show();
+    $(".notice-con").show(); //공지사항은 보여주고
+    $(",gallery-con").hide(); //갤러리는 숨긴다.
 })
-$(".tab-title ul li:nth-chlid(2)").click
+//갤러리 탭 버튼 클릭했을 때
+$(".tab-title2 ul li:first-child(2)").click
+(function(){
+    $(".notice-con").hide(); //공지사항은 숨기고
+    $(",gallery-con").show(); //갤러리는 보여준다.
+})
